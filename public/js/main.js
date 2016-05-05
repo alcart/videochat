352c121ab189181e18c3fddd3e9f082078862b59
alcart <alcartdelg@gmail.com>
2016-05-05 17:14:59 -0400

    Added files via upload

---
+++ public/js/main.js
@@ -0,0 +1,252 @@
+// JavaScript Document
+
+var $button = $('#button');
+var $window = $(document);
+
+var $login_page = $('.login_page');
+var $room_name = $('#room_name'); //Room Name Input
+var $username = $('#username'); //Username Input
+var $password = $('#room_password'); //Password Input
+
+var $chatPage = $('.chat_page');
+var $messages = $('.messages');
+var $connected = $('.connected');
+var $people = $('.people');
+var $inputMessage = $('.inputMessage');
+
+var socket = io();
+
+var usernameJSON;
+var connected = false;
+var inactive = false;
+var mp3 = document.createElement("AUDIO");
+mp3.setAttribute("src", "mp3/sounds-949-you-wouldnt-believe.mp3");
+
+function askPermission() {
+    if (window.Notification && Notification.permission !== "granted"){
+        Notification.requestPermission(function (status) {
+            if (Notification.permission !== status){
+                Notification.permission = status;
+            }
+        });
+    }
+}
+
+function showNotification(body, title, tag) {
+    var options = {
+        body: body,
+        icon: "img/ic_message_black_24dp_2x.png",
+        tag: tag
+    };
+    if (window.Notification && Notification.permission === "granted") {
+        var n = new Notification(title, options);
+        var timer = setTimeout(n.close.bind(n), 5000);
+        mp3.play();
+    }
+}
+// Scroll Bar Plugin
+(function($){
+    $(window).load(function(){
+        $(".msg").mCustomScrollbar({
+            axis: "y",
+            autoExpandScrollbar: true,
+            mouseWheel: {enable:true},
+            scrollButtons :{enable:false},
+            keyboard: {enable:true},
+            advanced: {updateOnSelectorChange: true},
+            theme: "light-thick",
+            callbacks: {
+                onSelectorChange: function () {
+                    $('.msg').mCustomScrollbar("scrollTo", "last",{
+                        scrollInertia:0,
+                        timeout:0
+                    });
+                }
+            }
+        });
+    });
+})(jQuery);
+// Focus Events
+{
+    // Room Name
+    $room_name.on("focus", function () {
+        $room_name.attr("placeholder", "");
+    });
+    $room_name.on("blur", function () {
+        $room_name.attr("placeholder", "Type Room Name")
+    });
+    // Password
+    $password.on("focus", function () {
+        $password.attr("placeholder", "");
+    });
+    $password.on("blur", function () {
+        $password.attr("placeholder", "Type Room Password")
+    });
+    // Username
+    $username.on("focus", function () {
+        $username.attr("placeholder", "");
+    });
+    $username.on("blur", function () {
+        $username.attr("placeholder", "Type Your Name")
+    });
+}
+
+
+
+// Functions
+{
+    function log (message) {
+        var $el = $('<li class="log">').text(message);
+        addMessageElement($el);
+    }
+    function addUsername(username) {
+        var $iconDiv = $('<li class="username"><span>'+username+'<i class="fa fa-video-camera" id="video"></i></li>');
+        $connected.append($iconDiv);
+    }
+    function sendMessage() {
+        var message = $inputMessage.val();
+        if (message && connected){
+            socket.emit("new message", message);
+            $inputMessage.val("");
+            addOutgoingMessage(message);
+        }
+    }
+    function addIncomeMessage(data) {
+        var icon = $("<i class='fa fa-video-camera' aria-hidden='true'></i>")
+            .data({username: data.username});
+        var $usernameDiv = $("<span class='username'/>")
+            .text(data.username+": ");
+        var $messageBody = $('<span class="messageBody"/>')
+            .text(data.message);
+        var $messageDiv = $('<li class="message"/>')
+            .append(icon, $usernameDiv, $messageBody);
+        icon.hide();
+        $usernameDiv.mouseenter(function () {
+            icon.show();
+        });
+        $usernameDiv.mouseleave(function () {
+            icon.hide();
+        });
+        $usernameDiv.click(function () {
+            var currentUrl = window.location.href;
+            var newUrl = currentUrl+randomToken();
+            usernameJSON = $(this).val();
+            var win = window.open(newUrl, "VideoCall", "fullscreen=yes,resizable=yes");
+            if (win){
+                win.focus();
+            }
+            else{
+                alert("Deactivate Pop-up Blocker");
+            }
+        });
+        addMessageElement($messageDiv);
+    }
+    function addMessageElement (el) {
+        var $el = $(el);
+        $messages.append($el);
+    }
+
+    function addOutgoingMessage(message) {
+        var $usernameDiv = $('<span class="my_username"/>')
+            .text(": Me");
+        var $messageBody = $('<span class="messageBody"/>')
+            .text(message);
+        var $messageDiv = $('<li class="my_message"/>')
+            .append($messageBody, $usernameDiv);
+        addMessageElement($messageDiv);
+    }
+    function connectRoom(username, password, room_name) {
+        socket.emit("create or join", username, password, room_name);
+        $login_page.fadeOut();
+        $login_page.off('click');
+        $chatPage.show();
+        $inputMessage.focus();
+
+    }
+    function disconnectRoom() {
+        window.location.reload();
+    }
+    function randomToken() {
+        return Math.floor((1 + Math.random()) * 1e16).toString(16).substring(1);
+    }
+}
+// Keyboard Events
+{
+    $window.keydown(function (event) {
+        if (!(event.ctrlKey || event.metaKey || event.altKey) && $chatPage.is(":visible")) {
+            $inputMessage.focus();
+        }
+        if (event.which === 13) {
+            var password = $password.val();
+            var room_name = $room_name.val();
+            var username = $username.val();
+
+            if ($chatPage.is(":visible")) {
+                sendMessage();
+            }
+            else if (username && password && room_name){
+                    connectRoom(username, password, room_name);
+                }
+            $password.val("");
+            $room_name.val("");
+            $username.val("");
+        }
+    });
+}
+
+socket.on("created", function(username){
+    connected = true;
+    log("Welcome To CS50 Chat");
+    addUsername(username);
+});
+
+socket.on("joined", function(username){
+    log("User: "+username+" has joined the room");
+    connected = true;
+
+});
+
+socket.on("new message", function(data){
+    if (inactive){
+        var body = data.username+": "+data.message;
+        showNotification(body, "New Message", "message");
+    }
+
+    addIncomeMessage(data);
+});
+
+socket.on("left", function (username) {
+   log(username+" Has Left Chat");
+});
+
+socket.on("log out", function () {
+    disconnectRoom();
+    console.log("Hello");
+});
+
+$window.ready(function(){
+    askPermission();
+    var logTime;
+    var idle;
+    $window.mousemove(function () {
+        inactive = false;
+        clearTimeout(logTime);
+        clearTimeout(idle);
+        if (connected) {
+            idle = setTimeout(function () {
+                inactive = true;
+            }, 15000);
+        }
+    });
+    $window.keydown(function () {
+        inactive = false;
+        clearTimeout(logTime);
+        clearTimeout(idle);
+        if (connected) {
+            idle = setTimeout(function () {
+                inactive = true;
+            }, 15000);
+        }
+    });
+
+});
---
+++ server.js
@@ -0,0 +1,108 @@
+/**
+ * Created by Alain on 20/04/2016.
+ */
+var express = require('express');
+var app = express();
+var securePort = 55555;
+var port = 3000;
+var rooms = [];
+//Http
+{
+
+    var http = require('http');
+    app.get("/:id", function (req, res) {
+        res.sendFile(__dirname+"/public/video.html");
+        console.log("routing");
+    });
+
+    app.use(express.static(__dirname + '/public'));
+
+    var server = http.createServer(app);
+
+    server.listen(port, function () {
+        console.log("Http server running at port: "+ port);
+    });}
+
+ 
+    var io = require("socket.io")(server);
+
+// Socket Events
+    io.on("connection", function (socket) {
+
+        socket.on("create or join", function (username, password, room_name){
+            if (rooms[room_name]) {
+                if (rooms[room_name] === password) {
+                    socket.join(room_name);
+                    socket.username = username;
+                    socket.room_name = room_name;
+                    socket.broadcast.to(socket.room_name).emit("joined", username);
+                    socket.emit("joined", username);
+                    var room = io.sockets.adapter.rooms;
+                    console.log(room);
+                    console.log("joined");
+                }
+            }
+            else {
+                socket.join(room_name);
+                rooms[room_name] = password;
+                socket.username = username;
+                socket.room_name = room_name;
+                socket.emit("created", username);
+                var room = io.sockets.adapter.rooms;
+                console.log(room);
+                console.log("created");
+            }
+        });
+        socket.on("new message", function (msg) {
+            socket.broadcast.to(socket.room_name).emit("new message", {
+                username: socket.username,
+                message: msg
+            });
+        });
+        socket.on("disconnect", function () {
+            socket.broadcast.to(socket.room_name).emit("left", socket.username);
+            socket.to(socket.id).emit("log out");
+            socket.disconnect();
+            var room = io.sockets.adapter.rooms[socket.room_name];
+            if (!room){
+                if (rooms[socket.room_name]) {
+                    delete rooms[socket.room_name];
+                }
+            }
+            console.log("disconnect");
+            console.log(rooms);
+        });
+
+        // Video Room Events
+
+        socket.on("create or join video", function (username) {
+            var numClients = io.of('/').adapter.rooms[socket.room_name].length;
+            
+            if (numClients == 0) {
+                socket.video_room = room;
+                socket.join(room);
+                socket.emit("created");
+            } else if (numClients === 1) {
+                socket.broadcast.to(room).emit("join", room);
+                socket.video_room = room;
+                socket.join(room);
+                socket.emit("joined");
+            } else {
+                socket.emit("full");
+            }
+        });
+
+        socket.on('message', function (message) {
+            console.log("got message", message);
+            socket.broadcast.to(socket.video_room).emit('message', message);
+        });
+
+    });
+
+
+
+
+
+
+
+
